9f186150fba892d127131812e7e6e6a4c55bd151
luiz <uxluiz@gmail.com>
2018-07-23 16:57:28 -0300

    Inclusão do JS mais Còdigo JS

---
+++ client/app/index.js
@@ -0,0 +1,8 @@
+//Cria um array que utiliza a API do DOM que vasculha os seletores utilizano campos CSS
+var campos = [
+    document.querySelector('#data'), 
+    document.querySelector('#quantidade'), 
+    document.querySelector('#valor')
+];
+//Imprime os objetos para saida
+console.log(campos);
